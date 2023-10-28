bd636ad7cdbef332a589efd9044029ba71dde9b5
0xilis <109871561+0xilis@users.noreply.github.com>
2023-10-27 21:41:24 -0400

    TWRequest.m

---
+++ TWRequest.m
@@ -0,0 +1,31 @@
+#import "TWRequest.h"
+
+@implementation TWRequest
+-(instancetype)initWithURL:(NSURL*)url parameters:(id)param requestMethod:(NSInteger)req {
+ return [super init];
+}
+-(void)addMultiPartData:(id)data withName:(id)name type:(id)type {
+ return;
+}
+-(void)dealloc {
+ [super dealloc];
+}
+-(void)performRequestWithHandler:(id)handler {
+ return;
+}
+-(NSURL *)URL {
+ return 0;
+}
+-(id)account {
+ return 0;
+}
+-(void)setAccount:(id)account {
+ return;
+}
+-(id)signedURLRequest {
+ return;
+}
+-(id)parameters {
+ return 0;
+}
+@end
